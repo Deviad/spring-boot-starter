c73c9418214652949789de4dca38620244a8bea7
Davide Pugliese <dvd.pugliese@gmail.com>
2017-08-08 05:22:04 +0300

    added support for livereload and monitoring of twig files

--- src/main/java/com/davidepugliese/springbootgradletest/controllers/IndexController.java
+++ src/main/java/com/davidepugliese/springbootgradletest/controllers/IndexController.java
@@ -1,13 +1,13 @@
 package com.davidepugliese.springbootgradletest.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
+
 //@RequestMapping("/user")
 public class IndexController {
     @RequestMapping({"", "/", "/index"})
